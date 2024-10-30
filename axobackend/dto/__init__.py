88895f39b31812305ef544047ad530959c93315e
fatimacm <19690338@tecvalles.mx>
2024-10-29 21:00:37 -0600

    Models added

--- axobackend/dto/__init__.py
+++ axobackend/dto/__init__.py
@@ -22,3 +22,50 @@ class CreateUserDTO(BaseModel):
 class AuthenticationAttemptDTO(BaseModel):
     username:str
     password:str
+
+class VirtualEnviromentDTO(BaseModel):
+    ve_id: Optional[str] =""
+    user_id: Optional[str] =""
+    cpu: str
+    memory: str
+    name: str
+    
+class CreateVirtualEnviromentDTO(BaseModel):
+    cpu: str
+    memory: str
+    name: str
+
+class EndpointDTO(BaseModel):
+    e_id: Optional[str] = ""
+    cpu: str
+    memory: str
+    
+class CreateEndpointDTO(BaseModel):
+    cpu: str
+    memory: str
+    
+class AxoObjectShadowDTO(BaseModel):
+    axos_id: Optional[str] = ""
+    created_at: Optional[str] = ""
+
+class AxoObjectDTO(BaseModel):
+    axo_id: Optional[str] = ""
+    created_at: Optional[str] = ""
+    
+class CodeDTO(BaseModel):
+    code_id: Optional[str] = ""
+    code: str
+    created_at: Optional[str] = ""
+    
+class CreateCodeDTO(BaseModel):
+    code: str
+    
+class ResultDTO(BaseModel):
+    result_id: Optional[str] = ""
+    hash:str
+    created_at: Optional[str] = ""
+    
+class TaskDTO(BaseModel):
+    task_id: Optional[str] = ""
+    created_at: Optional[str] = ""
+    
